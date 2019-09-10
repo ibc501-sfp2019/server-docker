c054ef40b638f510140df27c2d878fa2943a3a56
IC-H <insectchung@naver.com>
2019-09-10 20:31:34 +0900

    【Gate】基本的なEnum作成

---
+++ gate_code/common/__init__.py
@@ -0,0 +1,4 @@
+from .enum import __all__ as enum_all
+
+__all__ = []
+__all__ += enum_all 
---
+++ gate_code/common/enum/__init__.py
@@ -0,0 +1,5 @@
+from .http_enum import HttpMethods, HttpResponses
+
+__all__ = [
+    'HttpMethods', 'HttpResponses',
+]
