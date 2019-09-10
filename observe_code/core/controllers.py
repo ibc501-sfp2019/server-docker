5aa5b9ce4a8174cb160dffacf3ce06b54e3e5dd7
IC-H <insectchung@naver.com>
2019-09-10 22:31:38 +0900

    【Observe】位置取得API作成

---
+++ observe_code/core/controllers.py
@@ -0,0 +1,31 @@
+from django.http import JsonResponse
+from django.db import IntegrityError
+from django.utils import timezone
+from common.enum import HttpResponses
+from .models import Plays, PlayStatus, Balls, Locations
+
+def get_balls_location_list(request):
+    status = HttpResponses.OK.value
+    data = []
+    try:
+        play = Plays.objects.filter(id=request.GET.get('play_id')).filter(status=PlayStatus.IN_PROGRESS.value).first()
+        print(play)
+        now = timezone.now()
+        for ball in Balls.objects.filter(play=play):
+            print(ball)
+            locations = []
+            for l in Locations.objects.filter(ball=ball).filter(time__gte=now).order_by('time')[:50]:
+                locations.append(l.to_json())
+            
+            data.append({
+                'ball_id'   : ball.id,
+                'data'      : locations
+            })
+    except Plays.DoesNotExist:
+        status = HttpResponses.FORBIDDEN.value
+    except IntegrityError:
+        status = HttpResponses.INTERNER_SERVER_ERROR.value
+    return JsonResponse({
+        'status' : status,
+        'data'   : data
+    })
